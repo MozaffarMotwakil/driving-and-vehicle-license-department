61b4d8a7a649b3547398e6cf8155e7578a24c846
Mozaffar Motwakil Khider Salman <mozaffarmotwakil@gmail.com>
2025-08-05 07:27:21 +0200

    Fix: Clarify the names of the properties

--- DVLD.Entities/clsTestTypeEntity.cs
+++ DVLD.Entities/clsTestTypeEntity.cs
@@ -4,7 +4,7 @@ namespace DVLD.Entities
 {
     public class clsTestTypeEntity
     {
-        public int ID { get; set; }
+        public int TypeID { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public float Fees { get; set; }
@@ -13,7 +13,7 @@ namespace DVLD.Entities
 
         public clsTestTypeEntity (int ID, string Title, string Description, float Fees)
         {
-            this.ID = ID;
+            this.TypeID = ID;
             this.Title = Title;
             this.Description = Description;
             this.Fees = Fees;
