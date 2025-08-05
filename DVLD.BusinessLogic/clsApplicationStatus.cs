67f4f56ae16cf3f9144ed17998bea5476d9092e9
Mozaffar Motwakil Khider Salman <mozaffarmotwakil@gmail.com>
2025-08-05 07:32:08 +0200

    Feat: Add "ApplicationStatus" class in DAL, Entities Layer, BLL

---
+++ DVLD.BusinessLogic/clsApplicationStatus.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using DVLD.DataAccess;
+using System.Data;
+using DVLD.Entities;
+
+namespace DVLD.BusinessLogic
+{
+    public class clsApplicationStatus
+    {
+        public int StatusID { get; set; }
+        public string StatusName { get; set; }
+
+        private clsApplicationStatus(clsApplicationStatusEntity applicationStatusEntity)
+        {
+            this.StatusID = applicationStatusEntity.StatusID;
+            this.StatusName = applicationStatusEntity.StatusName;
+        }
+
+        public static DataTable GetAllApplicationStatuses()
+        {
+            return clsApplicationStatusData.GetAllApplicationStatuses();
+        }
+
+        public static clsApplicationStatus Find(int ApplicationStatusID)
+        {
+            return new clsApplicationStatus(clsApplicationStatusData.FindApplicationStatusByID(ApplicationStatusID));
+        }
+
+        public static clsApplicationStatus Find(string ApplicationStatusName)
+        {
+            return new clsApplicationStatus(clsApplicationStatusData.FindApplicationStatusByName(ApplicationStatusName));
+        }
+
+    }
+}
---
+++ DVLD.Entities/clsApplicationStatusEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DVLD.Entities
+{
+    public class clsApplicationStatusEntity
+    {
+        public int StatusID { get; set; }
+        public string StatusName { get; set; }
+
+        public clsApplicationStatusEntity() { }
+
+        public clsApplicationStatusEntity(int StatusID, string StatusName) 
+        {
+            this.StatusID = StatusID;
+            this.StatusName = StatusName;
+        }
+
+    }
+}
