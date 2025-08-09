2d2f418aea0f09f3351209644a6df689aa6f5d56
Mozaffar Motwakil Khider Salman <mozaffarmotwakil@gmail.com>
2025-08-09 12:04:18 +0200

    Refactor: move methods to clsBaseManage and clsBaseManageWithFilter

--- DVLD.WinForms/Utils/clsFormHelper.cs
+++ DVLD.WinForms/Utils/clsFormHelper.cs
@@ -9,86 +9,6 @@ namespace DVLD.WinForms.Utils
 {
     public static class clsFormHelper
     {
-        public static int RefreshDataGridViewWithFilter(DataGridView dataGridView, DataView DataSource, string FilterColumn, string FilterText)
-        {
-            DataView list = (DataView)dataGridView.DataSource;
-
-            if (!FilterColumn.EndsWith("ID") && FilterColumn != "IsActive")
-            {
-                list.RowFilter = $"{FilterColumn} LIKE '{FilterText}%'";
-            }
-            else
-            {
-                if (!string.IsNullOrWhiteSpace(FilterText))
-                {
-                    list.RowFilter = $"{FilterColumn} = {FilterText}";
-                }
-                else
-                {
-                    // If ID filter is empty, show all people again.
-                    // Why: To avoid an empty list when the ID filter is cleared.
-                    DataSource.RowFilter = string.Empty;
-                    return RefreshDataGridView(dataGridView, DataSource);
-                }
-            }
-
-            // The DataSource is updated with the filtered DataView.
-            // Why: Filtering on the DataView directly is efficient as it works on the
-            // already loaded data, avoiding re-querying the database just for filtering.
-            return RefreshDataGridView(dataGridView, list);
-        }
-
-        public static int RefreshDataGridView(DataGridView dataGridView, object DataSource)
-        {
-            dataGridView.DataSource = DataSource;
-            return dataGridView.RowCount;
-        }
-
-        public static void ReapplyAndHighlightFilterText(TextBox textBox)
-        {
-            if (!string.IsNullOrEmpty(textBox.Text))
-            {
-                string temp = textBox.Text;
-                textBox.Text = string.Empty;
-                textBox.Text = temp;
-
-                textBox.SelectionStart = 0;
-                textBox.SelectionLength = textBox.TextLength;
-            }
-        }
-
-        /// <summary>
-        /// Select the entire row where the right mouse button was pressed, and check the selected is not column.
-        /// rather than selecting a single cell because the context menu is on the record, not the cell.
-        /// </summary>
-        public static void SelectEntireRow(DataGridView dataGridView, DataGridViewCellMouseEventArgs e)
-        {
-            if ((e.Clicks == 2 || e.Button == MouseButtons.Right) && e.RowIndex >= 0)
-            {
-                dataGridView.ClearSelection();
-                dataGridView.Rows[e.RowIndex].Selected = true;
-                dataGridView.CurrentCell = dataGridView.SelectedRows[0].Cells[0];
-            }
-        }
-
-        public static void DeselectCellsAndRows(DataGridView dataGridView, MouseEventArgs e)
-        {
-            DataGridView.HitTestInfo hit = dataGridView.HitTest(e.X, e.Y);
-
-            if (e.Button == MouseButtons.Right || e.Button == MouseButtons.Left && hit.Type == DataGridViewHitTestType.None)
-            {
-                foreach (DataGridViewCell cell in dataGridView.SelectedCells)
-                {
-                    cell.Selected = false;
-                }
-
-                foreach (DataGridViewRow row in dataGridView.SelectedRows)
-                {
-                    row.Selected = false;
-                }
-            }
-        }
-
         public static int GetSelectedRowID(DataGridView dataGridView)
         {
             if (dataGridView.SelectedRows.Count > 0)
@@ -132,9 +52,19 @@ namespace DVLD.WinForms.Utils
 
         public static void SetDefaultValuesToCountriesComboBox(ComboBox countriesComboBox)
         {
-            countriesComboBox.Items.Add("None");
+            countriesComboBox.Items.Add("All");
             countriesComboBox.Items.AddRange(clsAppSettings.GetCountries());
             countriesComboBox.SelectedIndex = 0;
+        }
+        
+        public static void SetLicenseClassesToComboBox(ComboBox licenseClassesComboBox)
+        {
+            DataTable licenseClasses = clsLicenseClass.GetAllLicenseClasses();
+
+            foreach (DataRow licenseClass in licenseClasses.Rows)
+            {
+                licenseClassesComboBox.Items.Add(licenseClass["ClassName"]);
+            }
         }
 
         public static Image GetDefaultPersonImage(clsPerson.enGender gender)
