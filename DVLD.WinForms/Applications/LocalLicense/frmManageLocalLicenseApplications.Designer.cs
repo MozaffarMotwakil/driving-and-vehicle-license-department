8fc2e82e681dd4eeab7d1171d5d3927f1deef1ac
Mozaffar Motwakil Khider Salman <mozaffarmotwakil@gmail.com>
2025-08-10 13:42:56 +0200

    Feat: add "frmManageLocalLicenseApplications" form with design and implementation

---
+++ DVLD.WinForms/Applications/LocalLicense/frmManageLocalLicenseApplications.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace DVLD.WinForms.Applications.LocalLicense
+{
+    partial class frmManageLocalLicenseApplications
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.recordsListContextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.showDetailsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cancleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.scheduleTestsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.PhoneCallToolStripMenuItem = new System.Windows.Forms.ToolStripSeparator();
+            this.issueDrivingLicenseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.showLicenseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
+            this.showPersonLicensesHistoryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.formContextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.addNewUserToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.cbApplicationStatus = new System.Windows.Forms.ComboBox();
+            this.cbDrivingClass = new System.Windows.Forms.ComboBox();
+            this.nudPassedTests = new System.Windows.Forms.NumericUpDown();
+            this.dtpApplicationDate = new System.Windows.Forms.DateTimePicker();
+            this.recordsListContextMenuStrip.SuspendLayout();
+            this.formContextMenuStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudPassedTests)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // recordsListContextMenuStrip
+            // 
+            this.recordsListContextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showDetailsToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.editToolStripMenuItem,
+            this.deleteToolStripMenuItem,
+            this.cancleToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.scheduleTestsToolStripMenuItem,
+            this.PhoneCallToolStripMenuItem,
+            this.issueDrivingLicenseToolStripMenuItem,
+            this.toolStripSeparator3,
+            this.showLicenseToolStripMenuItem,
+            this.toolStripSeparator4,
+            this.showPersonLicensesHistoryToolStripMenuItem});
+            this.recordsListContextMenuStrip.Name = "contextMenuStrip1";
+            this.recordsListContextMenuStrip.Size = new System.Drawing.Size(262, 338);
+            // 
+            // showDetailsToolStripMenuItem
+            // 
+            this.showDetailsToolStripMenuItem.Image = global::DVLD.WinForms.Properties.Resources.PersonDetails_32;
+            this.showDetailsToolStripMenuItem.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.showDetailsToolStripMenuItem.Name = "showDetailsToolStripMenuItem";
+            this.showDetailsToolStripMenuItem.Size = new System.Drawing.Size(261, 38);
+            this.showDetailsToolStripMenuItem.Text = "Show Details";
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(258, 6);
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.Image = global::DVLD.WinForms.Properties.Resources.edit_32;
+            this.editToolStripMenuItem.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(261, 38);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Image = global::DVLD.WinForms.Properties.Resources.Delete_32_2;
+            this.deleteToolStripMenuItem.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(261, 38);
+            this.deleteToolStripMenuItem.Text = "Delete";
+            // 
+            // cancleToolStripMenuItem
+            // 
+            this.cancleToolStripMenuItem.Image = global::DVLD.WinForms.Properties.Resources.Delete_32;
+            this.cancleToolStripMenuItem.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.cancleToolStripMenuItem.Name = "cancleToolStripMenuItem";
+            this.cancleToolStripMenuItem.Size = new System.Drawing.Size(261, 38);
+            this.cancleToolStripMenuItem.Text = "Cancel";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(258, 6);
+            // 
+            // scheduleTestsToolStripMenuItem
+            // 
+            this.scheduleTestsToolStripMenuItem.Image = global::DVLD.WinForms.Properties.Resources.Schedule_Test_32;
+            this.scheduleTestsToolStripMenuItem.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.scheduleTestsToolStripMenuItem.Name = "scheduleTestsToolStripMenuItem";
+            this.scheduleTestsToolStripMenuItem.Size = new System.Drawing.Size(261, 38);
+            this.scheduleTestsToolStripMenuItem.Text = "Schedule Tests";
+            // 
+            // PhoneCallToolStripMenuItem
+            // 
+            this.PhoneCallToolStripMenuItem.Name = "PhoneCallToolStripMenuItem";
+            this.PhoneCallToolStripMenuItem.Size = new System.Drawing.Size(258, 6);
+            // 
+            // issueDrivingLicenseToolStripMenuItem
+            // 
+            this.issueDrivingLicenseToolStripMenuItem.Image = global::DVLD.WinForms.Properties.Resources.IssueDrivingLicense_32;
+            this.issueDrivingLicenseToolStripMenuItem.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.issueDrivingLicenseToolStripMenuItem.Name = "issueDrivingLicenseToolStripMenuItem";
+            this.issueDrivingLicenseToolStripMenuItem.Size = new System.Drawing.Size(261, 38);
+            this.issueDrivingLicenseToolStripMenuItem.Text = "Issue Driving License (First Time)";
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(258, 6);
+            // 
+            // showLicenseToolStripMenuItem
+            // 
+            this.showLicenseToolStripMenuItem.Image = global::DVLD.WinForms.Properties.Resources.License_View_32;
+            this.showLicenseToolStripMenuItem.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.showLicenseToolStripMenuItem.Name = "showLicenseToolStripMenuItem";
+            this.showLicenseToolStripMenuItem.Size = new System.Drawing.Size(261, 38);
+            this.showLicenseToolStripMenuItem.Text = "Show License";
+            // 
+            // toolStripSeparator4
+            // 
+            this.toolStripSeparator4.Name = "toolStripSeparator4";
+            this.toolStripSeparator4.Size = new System.Drawing.Size(258, 6);
+            // 
+            // showPersonLicensesHistoryToolStripMenuItem
+            // 
+            this.showPersonLicensesHistoryToolStripMenuItem.Image = global::DVLD.WinForms.Properties.Resources.PersonLicenseHistory_32;
+            this.showPersonLicensesHistoryToolStripMenuItem.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.showPersonLicensesHistoryToolStripMenuItem.Name = "showPersonLicensesHistoryToolStripMenuItem";
+            this.showPersonLicensesHistoryToolStripMenuItem.Size = new System.Drawing.Size(261, 38);
+            this.showPersonLicensesHistoryToolStripMenuItem.Text = "Show Person Licenses History";
+            // 
+            // formContextMenuStrip
+            // 
+            this.formContextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addNewUserToolStripMenuItem});
+            this.formContextMenuStrip.Name = "formContextMenuStrip";
+            this.formContextMenuStrip.Size = new System.Drawing.Size(188, 26);
+            // 
+            // addNewUserToolStripMenuItem
+            // 
+            this.addNewUserToolStripMenuItem.Image = global::DVLD.WinForms.Properties.Resources.New_Application_64;
+            this.addNewUserToolStripMenuItem.Name = "addNewUserToolStripMenuItem";
+            this.addNewUserToolStripMenuItem.Size = new System.Drawing.Size(187, 22);
+            this.addNewUserToolStripMenuItem.Text = "Add New Application";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.pictureBox1.Image = global::DVLD.WinForms.Properties.Resources.Local_32;
+            this.pictureBox1.Location = new System.Drawing.Point(637, 43);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(54, 40);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 31;
+            this.pictureBox1.TabStop = false;
+            // 
+            // cbApplicationStatus
+            // 
+            this.cbApplicationStatus.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.cbApplicationStatus.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbApplicationStatus.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbApplicationStatus.FormattingEnabled = true;
+            this.cbApplicationStatus.Items.AddRange(new object[] {
+            "All",
+            "New",
+            "Completed",
+            "Cancelled"});
+            this.cbApplicationStatus.Location = new System.Drawing.Point(228, 62);
+            this.cbApplicationStatus.Name = "cbApplicationStatus";
+            this.cbApplicationStatus.Size = new System.Drawing.Size(128, 24);
+            this.cbApplicationStatus.TabIndex = 34;
+            this.cbApplicationStatus.Visible = false;
+            this.cbApplicationStatus.SelectedIndexChanged += new System.EventHandler(this.cbApplicationStatus_SelectedIndexChanged);
+            // 
+            // cbDrivingClass
+            // 
+            this.cbDrivingClass.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.cbDrivingClass.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbDrivingClass.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbDrivingClass.FormattingEnabled = true;
+            this.cbDrivingClass.Location = new System.Drawing.Point(228, 150);
+            this.cbDrivingClass.Name = "cbDrivingClass";
+            this.cbDrivingClass.Size = new System.Drawing.Size(221, 24);
+            this.cbDrivingClass.TabIndex = 33;
+            this.cbDrivingClass.Visible = false;
+            this.cbDrivingClass.SelectedIndexChanged += new System.EventHandler(this.cbDrivingClass_SelectedIndexChanged);
+            // 
+            // nudPassedTests
+            // 
+            this.nudPassedTests.Cursor = System.Windows.Forms.Cursors.Default;
+            this.nudPassedTests.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nudPassedTests.InterceptArrowKeys = false;
+            this.nudPassedTests.Location = new System.Drawing.Point(228, 92);
+            this.nudPassedTests.Maximum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.nudPassedTests.Name = "nudPassedTests";
+            this.nudPassedTests.ReadOnly = true;
+            this.nudPassedTests.Size = new System.Drawing.Size(128, 23);
+            this.nudPassedTests.TabIndex = 36;
+            this.nudPassedTests.Visible = false;
+            this.nudPassedTests.ValueChanged += new System.EventHandler(this.nudPassedTests_ValueChanged);
+            // 
+            // dtpApplicationDate
+            // 
+            this.dtpApplicationDate.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtpApplicationDate.Location = new System.Drawing.Point(228, 121);
+            this.dtpApplicationDate.Name = "dtpApplicationDate";
+            this.dtpApplicationDate.Size = new System.Drawing.Size(221, 23);
+            this.dtpApplicationDate.TabIndex = 37;
+            this.dtpApplicationDate.Visible = false;
+            this.dtpApplicationDate.ValueChanged += new System.EventHandler(this.dtpApplicationDate_ValueChanged);
+            this.dtpApplicationDate.VisibleChanged += new System.EventHandler(this.dtpApplicationDate_VisibleChanged);
+            // 
+            // frmManageLocalLicenseApplications
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1184, 561);
+            this.ContextMenuStrip = this.formContextMenuStrip;
+            this.Controls.Add(this.dtpApplicationDate);
+            this.Controls.Add(this.cbDrivingClass);
+            this.Controls.Add(this.nudPassedTests);
+            this.Controls.Add(this.cbApplicationStatus);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "frmManageLocalLicenseApplications";
+            this.Text = "Manage Local License Applications";
+            this.Load += new System.EventHandler(this.frmManageLocalLicenseApplications_Load);
+            this.Controls.SetChildIndex(this.pictureBox1, 0);
+            this.Controls.SetChildIndex(this.cbApplicationStatus, 0);
+            this.Controls.SetChildIndex(this.nudPassedTests, 0);
+            this.Controls.SetChildIndex(this.cbDrivingClass, 0);
+            this.Controls.SetChildIndex(this.dtpApplicationDate, 0);
+            this.recordsListContextMenuStrip.ResumeLayout(false);
+            this.formContextMenuStrip.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudPassedTests)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip recordsListContextMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem showDetailsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cancleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem scheduleTestsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator PhoneCallToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem issueDrivingLicenseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripMenuItem showLicenseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator4;
+        private System.Windows.Forms.ToolStripMenuItem showPersonLicensesHistoryToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip formContextMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem addNewUserToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox cbApplicationStatus;
+        private System.Windows.Forms.ComboBox cbDrivingClass;
+        private System.Windows.Forms.NumericUpDown nudPassedTests;
+        private System.Windows.Forms.DateTimePicker dtpApplicationDate;
+    }
+}
---
+++ DVLD.WinForms/Applications/LocalLicense/frmManageLocalLicenseApplications.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using DVLD.BusinessLogic;
+using DVLD.WinForms.BaseForms;
+using DVLD.WinForms.Properties;
+using DVLD.WinForms.Utils;
+
+namespace DVLD.WinForms.Applications.LocalLicense
+{
+    internal partial class frmManageLocalLicenseApplications : frmBaseManageWithFilter
+    {
+        public frmManageLocalLicenseApplications() : base(clsLocalLicenseApplication.GetAllLocalLicenseApplications())
+        {
+            InitializeComponent();
+        }
+
+        private void frmManageLocalLicenseApplications_Load(object sender, EventArgs e)
+        {
+            _SetValuesToBaseFormControls();
+            _SetDefaultValuesToLicenseClasses_ComboBox();
+            _SetDefaultValuesToApplicationDate_DateTimePicker();
+        }
+
+        private void _SetDefaultValuesToApplicationDate_DateTimePicker()
+        {
+            dtpApplicationDate.MinDate = clsLocalLicenseApplication.GetMinimumApplicationDate();
+            dtpApplicationDate.MaxDate = clsLocalLicenseApplication.GetMaximumApplicationDate();
+            base.FilterText = string.Empty;
+        }
+
+        private void _SetDefaultValuesToLicenseClasses_ComboBox()
+        {
+            cbDrivingClass.Items.Add("All");
+            clsFormHelper.SetLicenseClassesToComboBox(cbDrivingClass);
+            cbDrivingClass.SelectedIndex = 0;
+        }
+
+        private void _SetValuesToBaseFormControls()
+        {
+            base.FormTitle = "Manage Local Driving License Applications";
+            base.FormLogo = Resources.Applications;
+            base.RecordsList.ContextMenuStrip = recordsListContextMenuStrip;
+            base.AddNewRecordButtonBackgroumd = Resources.New_Application_64;
+            base.AddNewRecordButtonBackgroumdLayout = System.Windows.Forms.ImageLayout.Zoom;
+            base.FilterColumns.AddRange(
+                new object[] {
+                    "L.D.L.App ID",
+                    "Driving Class",
+                    "National No",
+                    "Full Name",
+                    "Application Date",
+                    "Passed Tests",
+                    "Status"
+                }
+            );
+        }
+
+        protected override DataTable GetDataSource()
+        {
+            return clsLocalLicenseApplication.GetAllLocalLicenseApplications();
+        }
+
+        protected override void UpdateFilterControlsVisibility()
+        {
+            SetFilterColumnValue();
+
+            base.FilterTextControlVisible = cbDrivingClass.Visible = dtpApplicationDate.Visible =
+                nudPassedTests.Visible = cbApplicationStatus.Visible = false;
+
+            switch (base.SelectedFilterColumn)
+            {
+                case "ClassName":
+                    cbDrivingClass.Location = new Point(cbDrivingClass.Location.X, 178);
+                    cbDrivingClass.Visible = true;
+                    cbDrivingClass.SelectedIndex = 0;
+                    break;
+                case "ApplicationDate":
+                    dtpApplicationDate.Location = new Point(dtpApplicationDate.Location.X, 179);
+                    dtpApplicationDate.Visible = true;
+
+                    if (dtpApplicationDate.Value == DateTime.Now.Date || dtpApplicationDate.Value == dtpApplicationDate.MaxDate)
+                    {
+                        base.FilterText = dtpApplicationDate.Value.ToString("yyyy/MM/dd");
+                    }
+
+                    dtpApplicationDate.Value = dtpApplicationDate.MaxDate > DateTime.Now.Date ? DateTime.Now.Date : dtpApplicationDate.MaxDate;
+                    break;
+                case "PassedTests":
+                    nudPassedTests.Location = new Point(nudPassedTests.Location.X, 179); ;
+                    nudPassedTests.Visible = true;
+
+                    if (nudPassedTests.Value == 0)
+                    {
+                        base.FilterText = nudPassedTests.Value.ToString();
+                    }
+                    
+                    nudPassedTests.Value = 0;
+                    break;
+                case "ApplicationStatusName":
+                    cbApplicationStatus.Location = new Point(cbApplicationStatus.Location.X, 178);
+                    cbApplicationStatus.Visible = true;
+                    cbApplicationStatus.SelectedIndex = 0;
+                    break;
+                default:
+                    base.DefaultFilterControlsVisibility();
+                    break;
+            }
+        }
+
+        protected override void SetFilterColumnValue()
+        {
+            base.SetFilterColumnValue();
+
+            switch (base.SelectedFilterColumn)
+            {
+                case "L.D.L.AppID":
+                    base.SelectedFilterColumn = "LocalDrivingLicenseApplicationID";
+                    break;
+                case "DrivingClass":
+                    base.SelectedFilterColumn = "ClassName";
+                    break;
+                case "Status":
+                    base.SelectedFilterColumn = "ApplicationStatusName";
+                    break;
+            }
+        }
+
+        private void cbDrivingClass_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            base.SetFilterTextFromComboBox(cbDrivingClass);
+        }
+
+        private void cbApplicationStatus_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            base.SetFilterTextFromComboBox(cbApplicationStatus);
+        }
+
+        private void dtpApplicationDate_ValueChanged(object sender, EventArgs e)
+        {
+            base.SetFilterTextFromDate(dtpApplicationDate.Value);
+        }
+
+        private void nudPassedTests_ValueChanged(object sender, EventArgs e)
+        {
+            base.FilterText = nudPassedTests.Value.ToString();
+        }
+
+        private void dtpApplicationDate_VisibleChanged(object sender, EventArgs e)
+        {
+            DateTime lastMaximumApplicationDate = clsLocalLicenseApplication.GetMaximumApplicationDate();
+
+            if (dtpApplicationDate.MaxDate != lastMaximumApplicationDate)
+            {
+                dtpApplicationDate.MaxDate = lastMaximumApplicationDate;
+            }
+        }
+
+    }
+}
