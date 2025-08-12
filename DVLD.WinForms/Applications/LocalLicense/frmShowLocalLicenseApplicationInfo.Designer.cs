02e81be9d2ed4052b796d9357267b7d017716bc9
Mozaffar Motwakil Khider Salman <mozaffarmotwakil@gmail.com>
2025-08-12 07:00:05 +0200

    Feat: add "frmShowLocalLicenseApplicationInfo" form

---
+++ DVLD.WinForms/Applications/LocalLicense/frmShowLocalLicenseApplicationInfo.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace DVLD.WinForms.Applications.LocalLicense
+{
+    partial class frmShowLocalLicenseApplicationInfo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCloseScreen = new System.Windows.Forms.Button();
+            this.ctrLocalLicenseApplicationInfo = new DVLD.WinForms.Applications.LocalLicense.ctrLocalLicenseApplicationInfo();
+            this.SuspendLayout();
+            // 
+            // btnCloseScreen
+            // 
+            this.btnCloseScreen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCloseScreen.BackgroundImage = global::DVLD.WinForms.Properties.Resources.Close_32;
+            this.btnCloseScreen.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnCloseScreen.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCloseScreen.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCloseScreen.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCloseScreen.Location = new System.Drawing.Point(585, 330);
+            this.btnCloseScreen.Name = "btnCloseScreen";
+            this.btnCloseScreen.Size = new System.Drawing.Size(102, 34);
+            this.btnCloseScreen.TabIndex = 24;
+            this.btnCloseScreen.Text = "     Close";
+            this.btnCloseScreen.UseVisualStyleBackColor = true;
+            this.btnCloseScreen.Click += new System.EventHandler(this.btnCloseScreen_Click);
+            // 
+            // ctrLocalLicenseApplicationInfo
+            // 
+            this.ctrLocalLicenseApplicationInfo.LocalLicenseApplication = null;
+            this.ctrLocalLicenseApplicationInfo.Location = new System.Drawing.Point(2, 1);
+            this.ctrLocalLicenseApplicationInfo.Name = "ctrLocalLicenseApplicationInfo";
+            this.ctrLocalLicenseApplicationInfo.Size = new System.Drawing.Size(693, 317);
+            this.ctrLocalLicenseApplicationInfo.TabIndex = 0;
+            // 
+            // frmShowLocalLicenseApplicationInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCloseScreen;
+            this.ClientSize = new System.Drawing.Size(699, 376);
+            this.Controls.Add(this.btnCloseScreen);
+            this.Controls.Add(this.ctrLocalLicenseApplicationInfo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frmShowLocalLicenseApplicationInfo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Show Local License Application Info";
+            this.Load += new System.EventHandler(this.frmShowLocalLicenseApplicationInfo_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ctrLocalLicenseApplicationInfo ctrLocalLicenseApplicationInfo;
+        private System.Windows.Forms.Button btnCloseScreen;
+    }
+}
---
+++ DVLD.WinForms/Applications/LocalLicense/frmShowLocalLicenseApplicationInfo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DVLD.BusinessLogic;
+using DVLD.WinForms.Utils;
+
+namespace DVLD.WinForms.Applications.LocalLicense
+{
+    public partial class frmShowLocalLicenseApplicationInfo : Form
+    {
+        private clsLocalLicenseApplication _LocalLicenseApplication;
+        public frmShowLocalLicenseApplicationInfo(int LocalLicenseApplicationID)
+        {
+            InitializeComponent();
+            this._LocalLicenseApplication = clsLocalLicenseApplication.Find(LocalLicenseApplicationID);
+        }
+
+        public frmShowLocalLicenseApplicationInfo(clsLocalLicenseApplication LocalLicenseApplication)
+        {
+            InitializeComponent();
+            this._LocalLicenseApplication = LocalLicenseApplication;
+        }
+
+        private void btnCloseScreen_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void frmShowLocalLicenseApplicationInfo_Load(object sender, EventArgs e)
+        {
+            if (_LocalLicenseApplication == null)
+            {
+                clsFormMessages.ShowError("Sorry, local license application not fount.");
+                this.Close();
+                return;
+            }
+
+            ctrLocalLicenseApplicationInfo.LoadLocalLicenseApplicationDataForDisplay(_LocalLicenseApplication);
+        }
+
+    }
+}
